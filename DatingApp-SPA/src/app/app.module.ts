01912517515124cb88e23be0acc585d714e4ddbc
Orlando Ruiz <33588855+oruiz622@users.noreply.github.com>
2020-09-02 16:48:23 -0400

    working on auth

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [AppComponent, ValueComponent],
+  imports: [BrowserModule, HttpClientModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [
+            StringLength(
+                25,
+                MinimumLength = 6,
+                ErrorMessage =
+                    "You must have a password at least 6 characters long and can be no longer than 25 characters.")
+        ]
+        public string Password { get; set; }
+    }
+}
